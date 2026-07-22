283c550fe7fc61e2e5295d33df2d66050c613b41
Tazik Shahjahan <tshahjah@uwaterloo.ca>
2026-07-22 01:19:21 -0700

    Custom tile art: hex tile PNGs extracted from a board image
    Number tokens removed and hexes masked with transparency.
    Maritime tile unchanged.

--- ui/src/pages/Tile.tsx
+++ ui/src/pages/Tile.tsx
@@ -2,12 +2,12 @@ import cn from "classnames";
 import Paper from "@mui/material/Paper";
 
 import "./Tile.scss";
-import brickTile from "../assets/tile_brick.svg";
-import desertTile from "../assets/tile_desert.svg";
-import grainTile from "../assets/tile_wheat.svg";
-import lumberTile from "../assets/tile_wood.svg";
-import oreTile from "../assets/tile_ore.svg";
-import woolTile from "../assets/tile_sheep.svg";
+import brickTile from "../assets/tile_brick.png";
+import desertTile from "../assets/tile_desert.png";
+import grainTile from "../assets/tile_wheat.png";
+import lumberTile from "../assets/tile_wood.png";
+import oreTile from "../assets/tile_ore.png";
+import woolTile from "../assets/tile_sheep.png";
 import maritimeTile from "../assets/tile_maritime.svg";
 import { SQRT3, tilePixelVector } from "../utils/coordinates";
 import {
